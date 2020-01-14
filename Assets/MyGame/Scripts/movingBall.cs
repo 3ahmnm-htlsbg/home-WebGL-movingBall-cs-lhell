811718ea784788a09c6ac2f19508e7dd870741ff
HellLisa <lisahell@nbgum-lhell03-MacBook-Pro.local>
2020-01-14 08:30:53 +0100

    finish GUI and Script

--- Assets/MyGame/Scripts/movingBall.cs
+++ Assets/MyGame/Scripts/movingBall.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class movingBall : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    public GameObject football;
+        [SerializeField] private GameObject[] footballPositions;
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+    private int RandomNumbers(){
+    
+            return Random.Range(0,4);
+    
+        }
         
-    }
+        
+    public void randomNumbersTwo(){
+    
+            football.transform.position = footballPositions[RandomNumbers()].transform.position;
+            
+        }
 }
